39219111312ee5c4424a80922679b52ccc3fe4b6
Shazin Muhammed <kpshazin786@gmail.com>
2026-05-07 13:36:45 +0530

    patch34

--- games/urls.py
+++ games/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
     path('register/',views.register,name='register'),
     path('',views.login,name='login'),
