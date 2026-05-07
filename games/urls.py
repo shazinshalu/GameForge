005a324b3bfa995ef90c3e920d1b0da27d500a0c
Shazin Muhammed <kpshazin786@gmail.com>
2026-05-07 13:08:46 +0530

    cupdated all errors

--- games/urls.py
+++ games/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from . import views
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
--- pcgames/settings.py
+++ pcgames/settings.py
@@ -49,7 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'pcgames.urls'
 
@@ -119,8 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
