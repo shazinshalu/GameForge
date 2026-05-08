9c0941e86ac12ad5c6b05342833815d6963289a9
Shazin Muhammed <kpshazin786@gmail.com>
2026-05-08 09:52:49 +0530

    img456

--- pcgames/urls.py
+++ pcgames/urls.py
@@ -23,7 +23,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('games.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 
 
